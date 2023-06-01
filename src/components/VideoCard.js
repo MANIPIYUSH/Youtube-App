f24985dfc32d1e16ae6f2d119ecad178fb69e6c5
PIYUSH MANI CHAUBEY <42182675+MANIPIYUSH@users.noreply.github.com>
2023-06-01 17:23:21 +0530

    integrate Api of youtube videos

---
+++ src/components/VideoCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function VideoCard({info}) {
+    console.log(info);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default VideoCard
---
+++ src/utils/constant.js
@@ -0,0 +1,5 @@
+
+const GOOGLE_API_KEY = "AIzaSyBdu7nmDc8aRWcIjenCBB_BqFRpOAZ3eu8";
+
+export const YOUTUBE_API_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+ GOOGLE_API_KEY;
+
