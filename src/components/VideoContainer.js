eeecbc5bf28588b9a6ccf7251e5f4f9b123ab847
PIYUSH MANI CHAUBEY <42182675+MANIPIYUSH@users.noreply.github.com>
2023-06-02 13:04:13 +0530

    resolve the youtube api problem

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import VideoCard from './VideoCard';
+import {YOUTUBE_API_URL} from '../utils/constant';
 
 function VideoContainer() {
+ const [videos, setVideos] = useState([]);
+  useEffect(()=>{
+     getVideos();
+  },[]);
+
+  const getVideos = async ()=>{
+    const data = await fetch( YOUTUBE_API_URL );
+    //console.log(data);
+    const json = await data.json();
+    // console.log(json);
+    setVideos(json.items);
+  }
+
+
   return (
-    <div>
-      <h1>videoContainer</h1>
+    <div className='flex flex-wrap'>
+      {videos.map(video => <VideoCard key ={video.id} info={video}/>)}
+    
     </div>
   )
 }
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,7 @@
 
-const GOOGLE_API_KEY = "AIzaSyBdu7nmDc8aRWcIjenCBB_BqFRpOAZ3eu8";
+const GOOGLE_API_KEY = "AIzaSyDV1GEwuVcsiVBcG_0GdRejol1Tqcx8smg";
 
-export const YOUTUBE_API_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+ GOOGLE_API_KEY;
+export const YOUTUBE_API_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" + GOOGLE_API_KEY;
+
+
 
