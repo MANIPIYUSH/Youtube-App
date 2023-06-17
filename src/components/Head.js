ba7a8b0b10d081889cb0764f422c41954e471aeb
piyush <piyush.chaubey@consolebit.com>
2023-06-17 23:51:17 +0530

    update head

--- src/components/Head.js
+++ src/components/Head.js
@@ -6,6 +6,7 @@ import { YOUTUBE_SEARCH_API } from "../utils/constant";
 const Head = ()=>{
 
   const[searchQuery,setSearchQuery] = useState("");
+  const[suggestions,setSuggestions] = useState([]);
        
   useEffect(()=>{
     //make an api call after every key press
@@ -33,7 +34,8 @@ const Head = ()=>{
     console.log(searchQuery)
   const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
   const json = await data.json();
-  console.log(json);
+  //console.log(json[1] );
+  setSuggestions(json[1]);
   
  }
 
@@ -49,19 +51,34 @@ const Head = ()=>{
             className="h-8 cursor-pointer" src="https://static.vecteezy.com/system/resources/previews/002/292/406/original/hamburger-menu-line-icon-free-vector.jpg" alt="menu-icon" />
             <img className="h-8 mx-2" src="https://lh3.googleusercontent.com/3zkP2SYe7yYoKKe47bsNe44yTgb4Ukh__rBbwXwgkjNRe4PykGG409ozBxzxkrubV7zHKjfxq6y9ShogWtMBMPyB3jiNps91LoNH8A=s500" alt="youtube-logo" />
           </div>
-         <div className="flex justify-center col-span-10">
-           <input className="w-1/2 border border-gray-400 p-2 rounded-l-full" type="text"
-             value={searchQuery}
-             onChange={(e)=>setSearchQuery(e.target.value)}
-           />
-           <button className="border border-gray-400 p-2 px-5 bg-gray-100 rounded-r-full">🔍</button>
-         </div>
+         <div className="col-span-10 px-10">  
+              <div>
+              <input className="w-1/2 border border-gray-400 p-2 rounded-l-full" type="text"
+                value={searchQuery}
+                onChange={(e)=>setSearchQuery(e.target.value)}
+               />
+               <button className="border border-gray-400 p-2 px-5 bg-gray-100 rounded-r-full">🔍</button>
+                </div> 
+         
+               <div className="fixed bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100">
+                   <ul> 
+                    {
+                      suggestions?.map((s)=>{<li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">{s}</li>})
+                    }
+                    
+                    
+                  </ul>
+               </div>
+           </div>
+
+
           <div className="col-span-1">
             <img className="h-8" src="https://static.vecteezy.com/system/resources/thumbnails/005/545/335/small/user-sign-icon-person-symbol-human-avatar-isolated-on-white-backogrund-vector.jpg" alt="user-icon" />
           </div>
 
 
-        </div>
+  </div>
+        
   )
 
 }
