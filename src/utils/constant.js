6de54c2909f774ee5fda0f6bf419de4f0d816845
piyush <piyush.chaubey@consolebit.com>
2023-06-21 16:24:35 +0530

    added the youtube search suggestion

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,5 +3,6 @@ const GOOGLE_API_KEY = "AIzaSyCeEDOqVNMWu4xYEyqTwlgcCa3AJwx_loQ";
 
 export const YOUTUBE_API_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOOGLE_API_KEY;
 
-export const YOUTUBE_SEARCH_API = "https://api.allorigins.win/get?url=https://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q&q=";
+export const YOUTUBE_SEARCH_API ="https://corsproxy.io/?https://clients1.google.com/complete/search?client=firefox&ds=yt&q="
+
 
